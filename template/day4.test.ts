a541f1c7a5bb960951ae0d2356d5a25cc62693db
François Delfort <francois.delfort@doctolib.com>
2022-12-04 09:07:13 +0100

    Template

---
+++ template/day4.test.ts
@@ -0,0 +1,11 @@
+import { xxx } from './day4'
+
+const input = ``
+
+// test('part 1 works', () => {
+//   expect(xxx(input)).toEqual(yyy)
+// })
+
+// test('part 2 works', () => {
+//   expect(xxx(input)).toEqual(yyy)
+// })
---
+++ template/day4.ts
@@ -0,0 +1,12 @@
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+export const xxx = (input: string) => {}
+
+// Part 1
+// const input = readFileSync(join(__dirname, 'input.txt'), 'utf8')
+// console.log(xxx(input))
+
+// Part 2
+// const input = readFileSync(join(__dirname, 'input.txt'), 'utf8')
+// console.log(sumOfPrioritiesOfBadgeItemsForEeach3ElfGroup(input))
